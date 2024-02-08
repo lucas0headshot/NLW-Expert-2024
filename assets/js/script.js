09951fc3b7bb81e0c6d44a9a99077bae9fb81e96
lucas0headshot <lucas0headshot@gmail.com>
2024-02-07 23:44:43 -0300

    Finalização e add aula 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,24 @@ function setupQuiz(quizData) {
             const resultElement = document.querySelector('#result span');
             function updateResult() {
                 resultElement.textContent = `${hits.size} de ${questionsQuantity}`; //Set the result content
+
+                if (hits.size === questionsQuantity) {
+                    document.querySelector('#result').classList.add('bounce');
+                }
             }
 
             updateResult();
 
 
             for (const question of quiz.questions) { 
-                const quizQuestionElement = templateElement.content.cloneNode(true); //Clone the question from template
+                const questionTemplate = templateElement.content.cloneNode(true);
+                const questionElement = questionTemplate.querySelector('div').cloneNode(true); //Clone the question from template
 
-                quizQuestionElement.querySelector('h3').textContent = question.title; //Set the question title
+                questionElement.querySelector('h3').textContent = question.title; //Set the question title
 
                 if (question.answers.length > 0) { //Ensure that question have answer
                     for (let answer of question.answers) {
-                        const answerElement = quizQuestionElement.querySelector('dl dt').cloneNode(true); //Clone the answer from question
+                        const answerElement = questionElement.querySelector('dl dt').cloneNode(true); //Clone the answer from question
 
                         answerElement.querySelector('span').textContent = answer.title; //Set the answer tile
                         answerElement.querySelector('input').setAttribute('name', `question-${quiz.questions.indexOf(question)}`); //Set the name of input radio to question index                        
@@ -37,18 +42,24 @@ function setupQuiz(quizData) {
                             hits.delete(question); //Check that question is already in hits
                             if (correctAnswer) {
                                 hits.add(question); //Increments the result
+                                answerElement.classList.add('correct', 'pulse'); //Add an class to visually mark the answer as correct
+                                questionElement.querySelectorAll('input[type = "radio"]').forEach((input) => {
+                                    input.disabled = true;
+                                });
+                            } else {
+                                answerElement.classList.add('incorrect', 'shake');
                             }
 
                             updateResult();
                         });
 
-                        quizQuestionElement.querySelector('dl').appendChild(answerElement); //Append answer into question
+                        questionElement.querySelector('dl').appendChild(answerElement); //Append answer into question
                     }
  
-                    quizQuestionElement.querySelector('dl dt').remove();
+                    questionElement.querySelector('dl dt').remove();
                 }
 
-                quizElement.appendChild(quizQuestionElement); //Append question into quiz
+                quizElement.appendChild(questionElement); //Append question into quiz
             }
         }
     }
